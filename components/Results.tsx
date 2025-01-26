73d95487c4f2ed8ed704e980699d714b5ec325da
Christal Bell <christalvictoriabell@gmail.com>
2025-01-25 22:04:16 -0800

    cleaning results

--- components/Results.tsx
+++ components/Results.tsx
@@ -10,6 +10,25 @@ interface Props {
 
 const Results = ({ results }: Props) => {
   const result = results[0];
+  // const opposite = [
+  //   result.meanings[0].antonyms[0],
+  //   result.meanings[0].antonyms[1],
+  // ];
+  // const similar = [
+  //   result.meanings[0].synonyms[0],
+  //   result.meanings[0].synonyms[1],
+  // ];
+  // const answer = [
+  //   {
+  //     defined: result.meanings[0].definitions[0].definition,
+  //     sentence: result.meanings[0].definitions[0].example,
+  //   },
+  //   {
+  //     defined: result.meanings[1].definitions[0].definition,
+  //     sentence: result.meanings[0].definitions[0].example,
+  //   },
+  // ];
+
   console.log(result);
 
   return (
@@ -24,33 +43,83 @@ const Results = ({ results }: Props) => {
         <Typography sx={{ color: COLORS.lavender }}>
           {result.phonetics[0].audio}
         </Typography>
-        <Typography sx={{ color: COLORS.black }}>
-          {result.meanings[0].partOfSpeech}{" "}
-        </Typography>
       </Box>
 
-      <Box className="middle" sx={{ color: COLORS.black }}>
-        <Typography sx={{ color: COLORS.darkGrey }}>Meaning</Typography>
-
-        <ul>
-          <li> {result.meanings[0].definitions[0].definition} </li>
-          <li> {result.meanings[1].definitions[0].definition} </li>
-        </ul>
-        <Typography sx={{ color: COLORS.purple }}>
-          <span style={{ color: COLORS.darkGrey }}>Synonyms</span>{" "}
-          {result.meanings[0].synonyms[0]}
-        </Typography>
-        <Typography sx={{ color: COLORS.purple }}>
-          <span style={{ color: COLORS.darkGrey }}>Antonyms</span> and info{" "}
-        </Typography>
-      </Box>
-
-      <Box className="footer">
+      {/* per full definition  */}
+      <Box>
         <Typography sx={{ color: COLORS.black }}>
-          <span style={{ color: COLORS.darkGrey }}>Source</span> and link
+          {result.meanings[0].partOfSpeech}
         </Typography>
+        <Box>
+          <Typography>Meaning </Typography>
+          <Box>
+            {result.meanings[0].definitions.map((answer) => {
+              return (
+                <ul key={result.word}>
+                  <li>
+                    <div key={result.word}>
+                      <Typography>{answer.definition}</Typography>
+
+                      <Typography>{answer.example}</Typography>
+                    </div>
+                  </li>
+                </ul>
+              );
+            })}
+          </Box>
+        </Box>
+
+        <Typography>Synonym </Typography>
+        <Typography>Antonym</Typography>
       </Box>
     </Box>
+
+    // <Box>
+
+    //   <Box className="middle" sx={{ color: COLORS.black }}>
+    //     <Typography sx={{ color: COLORS.darkGrey }}>Meaning</Typography>
+    //     {answer.map((meaning) => {
+    //       return (
+    //         <ul key={result.word}>
+    //           <li>
+    //             <Box>
+    //               <Typography>{meaning.defined}</Typography>
+    //               <Typography
+    //                 sx={{ marginLeft: "2rem", color: COLORS.darkGrey }}
+    //               >
+    //                 {meaning.sentence}
+    //               </Typography>
+    //             </Box>{" "}
+    //           </li>
+    //         </ul>
+    //       );
+    //     })}
+
+    //     <Typography style={{ color: COLORS.darkGrey }}>Synonyms</Typography>
+    //     {similar.map((synonym) => {
+    //       return (
+    //         <Typography key={result.word} sx={{ color: COLORS.purple }}>
+    //           {synonym}
+    //         </Typography>
+    //       );
+    //     })}
+
+    //     <Typography sx={{ color: COLORS.darkGrey }}>antonyms</Typography>
+    //     {opposite.map((antonym) => {
+    //       return (
+    //         <Typography key={result.word} sx={{ color: COLORS.purple }}>
+    //           {antonym}
+    //         </Typography>
+    //       );
+    //     })}
+    //   </Box>
+
+    //   <Box className="footer">
+    //     <Typography sx={{ color: COLORS.black }}>
+    //       <span style={{ color: COLORS.darkGrey }}>Source</span> and link
+    //     </Typography>
+    //   </Box>
+    // </Box>
   );
 };
 
--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,8 @@
 export type Meaning = {
   partOfSpeech: string;
-  synonyms: [];
-  antonyms: [];
-  definitions: [
-    {
-      definition: string;
-      example: string;
-    }
-  ];
+  synonyms: string[];
+  antonyms: string[];
+  definitions: [{ definition: string; example: string }];
 };
 
 export type Result = {
@@ -37,48 +32,4 @@ export type Result = {
 //   },
 //   { length: number },
 //   {
-//     Prototype: [
-//       {
-//         at: object;
-//         concat: object;
-//         constructor: object;
-//         copyWithin: object;
-//         entries: object;
-//         every: object;
-//         fill: object;
-//         filter: object;
-//         find: object;
-//         findIndex: object;
-//         findLast: object;
-//         findLastIndex: object;
-//         flat: object;
-//         flatMap: object;
-//         forEach: object;
-//         includes: object;
-//         indexOf: object;
-//         join: object;
-//         keys: object;
-//         lastIndexOf: { lastIndexOf: object; length: number };
-//         map: object;
-//         pop: object;
-//         push: object;
-//         reduce: object;
-//         reduceRight: object;
-//         reverse: object;
-//         shift: object;
-//         slice: object;
-//         some: object;
-//         sort: object;
-//         splice: object;
-//         toLocaleString: object;
-//         toReversed: object;
-//         toSorted: object;
-//         toSpliced: object;
-//         toString: object;
-//         unshift: object;
-//         values: object;
-//         with: object;
-//       }
-//     ];
-//   }
-// ];
+//    ]
