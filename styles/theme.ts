07b2add462a9d5fe34c88b300200f16be195e41f
Christal Bell <christalvictoriabell@gmail.com>
2025-04-17 22:46:12 -0700

    working on dark mode theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,6 +3,24 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   palette: {
+    mode: "light",
+    primary: {
+      main: "white",
+      light: "black",
+      dark: "#a547e8",
+      contrastText: "#a547e8",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "4rem",
+      fontWeight: 800,
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
     mode: "dark",
     primary: {
       main: "black",
